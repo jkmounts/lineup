5c46ca4f426244097c6779c50cf91b1cdf47cd2c
Jestin Mounts <jkmounts@gmail.com>
2021-01-26 17:44:26 -0500

    player-changer.js appears to be working as intended and changing HTML on page correctlygit add .

--- js/player-changer.js
+++ js/player-changer.js
@@ -1,12 +1,17 @@
 // Create a function to change displayed player info
 
-
+// Loops Through all drop-down menus on page by id
 for (let i=1; i<=11; i++){
     let currentPlayerSlot = document.querySelector(`#player-${i}-name`);
 
+// eventListener looks for changes in drop-down menus
     currentPlayerSlot.addEventListener('input', function(event) {
+        // Retrieves the 'value' of selected option which is equal to index of selected player in array in players.js
         let selectedPlayerIndex = event.target.value;
+        // Change HTML in our current div using attributes of selected player
         let currentHometownSlot = document.querySelector(`#player-${i} .hometown`);
         let currentPhotoSlot = document.querySelector(`#player-${i} .photo`);
+        currentHometownSlot.innerHTML = `${players[selectedPlayerIndex].hometown}`;
+        currentPhotoSlot.innerHTML = `<img src="${players[selectedPlayerIndex].photo}">`;
     });
 };
