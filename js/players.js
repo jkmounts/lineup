ce82ebc584fccc5f9179fcbd03a082a67d52d388
Jestin Mounts <jkmounts@gmail.com>
2021-01-26 19:40:38 -0500

    Added more players objects to players array and corresponding photos

--- js/players.js
+++ js/players.js
@@ -16,5 +16,96 @@ const players = [
         position: 'GK',
         hometown: 'Plano, IL',
         photo: 'img/lund.jpg'
+    },
+    {
+        name: 'Julia Ashley',
+        position: 'D',
+        hometown: '',
+        photo: 'img/ashley.jpg'
+    },
+    {
+        name: 'Jennifer Cudjoe',
+        position: 'M',
+        hometown: 'Ghana',
+        photo: 'img/cudjoe.jpg'
+    },
+    {
+        name: 'Caitlin Foord',
+        position: 'F',
+        hometown: 'Australia',
+        photo: 'img/foord.jpg'
+    },
+    {
+        name: 'Tobin Heath',
+        position: 'M',
+        hometown: 'Morristown, NJ',
+        photo: 'img/heath.jpg'
+    },
+    {
+        name: 'Alanna Kennedy',
+        position: 'D',
+        hometown: 'Australia',
+        photo: 'img/kennedy.jpg'
+    },
+    {
+        name: 'Cece Kizer',
+        position: 'F',
+        hometown: 'Overland Park, KS',
+        photo: 'img/kizer.jpg'
+    },
+    {
+        name: 'Cheyna Matthews',
+        position: 'F',
+        hometown: 'Jamaica',
+        photo: 'img/matthews.jpg'
+    },
+    {
+        name: 'Savannah McCaskill',
+        position: 'F',
+        hometown: 'Chapin, SC',
+        photo: 'img/mccaskill.jpg'
+    },
+    {
+        name: 'Katie McClure',
+        position: 'F',
+        hometown: 'Wichita, KS',
+        photo: 'img/mcclure.jpg'
+    },
+    {
+        name: 'Addisyn Merrick',
+        position: 'D',
+        hometown: 'Lee\'s Summit, MO',
+        photo: 'img/merrick.jpg'
+    },
+    {
+        name: 'Lauren Milliet',
+        position: 'M',
+        hometown: 'Durango, CO',
+        photo: 'img/milliet.jpg'
+    },
+    {
+        name: 'Yuki Nagasato',
+        position: 'F',
+        hometown: 'Japan',
+        photo: 'img/nagasato.jpg'
+    },
+    {
+        name: 'Christen Press',
+        position: 'F',
+        hometown: 'Los Angeles, CA',
+        photo: 'img/press.jpg'
+    },
+    {
+        name: 'Kaleigh Riehl',
+        position: 'D',
+        hometown: 'Fairfax Station, VA',
+        photo: 'img/riehl.jpg'
+    },
+    {
+        name: 'Erin Simon',
+        position: 'D',
+        hometown: 'Oakhurst, NJ',
+        photo: 'img/simon.jpg'
     }
+    
 ];
