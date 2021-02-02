7faeaf82a00fc2000a6d0c90860327ad2a3aef11
Jestin Mounts <jkmounts@gmail.com>
2021-02-02 12:21:56 -0500

    Added pop-up box to display html2canvas image. Need to make image resize to fit on page better.

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,6 +1,16 @@
 function screenshot() {
+    let lineupImage = document.querySelector('#lineupImage');
     let screenshotarea = document.getElementById("lineup");
-    html2canvas(screenshotarea).then(canvas => {
-        document.body.appendChild(canvas)
+    html2canvas(screenshotarea).then(function(canvas) {
+        if (lineupImage.hasChildNodes()) {
+            lineupImage.replaceChild(canvas, lineupImage.childNodes[0]);
+        } else {
+        lineupImage.appendChild(canvas);
+        };
     });
+    document.getElementById("screenshotContainer").style.display = "block";
 };
+
+function closeScreenshot() {
+    document.getElementById("screenshotContainer").style.display = "none";
+}
