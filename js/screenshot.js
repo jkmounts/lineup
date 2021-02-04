41b09a7e97bb70c7f18eb07cb249b413b033e4ce
Jestin Mounts <jkmounts@gmail.com>
2021-02-04 10:40:21 -0500

    Adjusted HTML2Canvas options to create visual lineup on mobile or desktop view.

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,7 +1,9 @@
 function screenshot() {
     let lineupImage = document.querySelector('#lineupImage');
     let screenshotarea = document.getElementById("lineup");
-    html2canvas(screenshotarea).then(function(canvas) {
+    html2canvas(screenshotarea, {
+            windowWidth: 1200,
+            width: 900}).then(function(canvas) {
         if (lineupImage.hasChildNodes()) {
             lineupImage.replaceChild(canvas, lineupImage.childNodes[0]);
         } else {
