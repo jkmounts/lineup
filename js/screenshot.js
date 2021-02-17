f460368f6b1439492bf14b4ba497f4e392168b8f
Jestin Mounts <jkmounts@gmail.com>
2021-02-16 19:34:59 -0500

    More edits to screenshot feature

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,20 +1,19 @@
 function screenshot() {
     let lineupImage = document.querySelector('#lineupImage');
-    let screenshotarea = document.getElementById("lineup");
+    let screenshotarea = document.querySelector('#lineup');
     html2canvas(screenshotarea, {
         windowWidth: 1200,
-        width: 900,
-        scrollX: 0,
-        scrollY: 0}).then((canvas) => {
+        scrollX: -window.scrollX,
+        scrollY: -window.scrollY}).then((canvas) => {
                 if (lineupImage.hasChildNodes()) {
                     lineupImage.replaceChild(canvas, lineupImage.childNodes[0]);
                 } else {
                     lineupImage.appendChild(canvas);
                 };
             });
-    document.getElementById("screenshotContainer").style.display = "block";
+    document.querySelector('#screenshotContainer').style.display = "block";
 };
 
 function closeScreenshot() {
-    document.getElementById("screenshotContainer").style.display = "none";
+    document.querySelector('#screenshotContainer').style.display = "none";
 }
