f76f2f398ba1e0cae7406e7acee83915bdad5dac
Jestin Mounts <jkmounts@gmail.com>
2021-02-16 19:33:47 -0500

    Used forEach loop to simplify code on playerChanger

--- js/player-changer.js
+++ js/player-changer.js
@@ -1,17 +1,11 @@
 // Create a function to change displayed player info
 
-// Loops Through all drop-down menus on page by id
-for (let i=1; i<=11; i++){
-    let currentPlayerSlot = document.querySelector(`#player-${i}-name`);
+playerDivs = document.querySelectorAll('.player')
 
-// eventListener looks for changes in drop-down menus
-    currentPlayerSlot.addEventListener('input', function(event) {
-        // Retrieves the 'value' of selected option which is equal to index of selected player in array in players.js
-        let selectedPlayerIndex = event.target.value;
-        // Change HTML in our current div using attributes of selected player
-        let currentHometownSlot = document.querySelector(`#player-${i} .hometown`);
-        let currentPhotoSlot = document.querySelector(`#player-${i} .photo`);
-        currentHometownSlot.innerHTML = `${players[selectedPlayerIndex].hometown}`;
-        currentPhotoSlot.innerHTML = `<img src="${players[selectedPlayerIndex].photo}" alt="${players[selectedPlayerIndex].name}">`;
-    });
-};
+// Create event listener on each select box within player divs
+playerDivs.forEach(div => (div.querySelector('select')).addEventListener('input', function(e) {
+    // Retrieves the 'value' of selected option which is equal to index of selected player in array in players.js
+    let selectedPlayerIndex = e.target.value;
+    (div.querySelector('.hometown')).innerHTML = `${players[selectedPlayerIndex].hometown}`;
+    (div.querySelector('.photo')).innerHTML = `<img src="${players[selectedPlayerIndex].photo}" alt="${players[selectedPlayerIndex].name}">`;
+}));
